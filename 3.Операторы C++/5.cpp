c704ef550980695b44b1fa65122e288523110db0
podlesnyy007 <114169264+podlesnyy007@users.noreply.github.com>
2022-11-25 02:53:40 +0400

    Update 5.cpp

--- 3.Операторы C++/5.cpp
+++ 3.Операторы C++/5.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 using namespace std;
+#include <cmath>
+
+double f(double &x) {
+    return ((x + 4) / (pow(x, 2) - 2)) + sqrt(pow(x, 3) - 1);
+}
 
 int main()
 {
-	double a, b, h;
-	cin >> a >> b >> h;
-	for (a; a <= b; a += h) {
-		cout << f(a);
-	}
-}
-
-double f(double x) {
-	return (x + 4) / (pow(x, 2) - 2) + sqrt(pow(x, 3) - 1);
+    double x1, x2, s;
+    cin >> x1 >> x2 >> s;
+    for (x1; x1 <= x2; x1 += s) {
+        cout << x1 << "\t" << f(x1) << endl;
+    }
 }
