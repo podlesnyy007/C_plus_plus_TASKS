2fa21316801973bfe63b9bc30c0897a588b39155
podlesnyy007 <114169264+podlesnyy007@users.noreply.github.com>
2022-11-25 08:50:53 +0400

    Update 5.cpp

--- 3.Операторы C++/5.cpp
+++ 3.Операторы C++/5.cpp
@@ -2,15 +2,21 @@
 using namespace std;
 #include <cmath>
 
-double f(double &x) {
-    return ((x + 4) / (pow(x, 2) - 2)) + sqrt(pow(x, 3) - 1);
+void f(double &x) {
+    if (x == sqrt(2) || x < 0)
+        cout << "функция не определена";
+    else
+        cout << ((x + 4) / (pow(x, 2) - 2)) + sqrt(pow(x, 3) - 1);
 }
 
 int main()
 {
+    setlocale(LC_ALL, "Rus");
     double x1, x2, s;
     cin >> x1 >> x2 >> s;
     for (x1; x1 <= x2; x1 += s) {
-        cout << x1 << "\t" << f(x1) << endl;
+        cout << x1 << "\t";
+        f(x1);
+        cout << endl;
     }
 }
