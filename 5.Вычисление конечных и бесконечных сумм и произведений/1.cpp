28ac5a0ca010db47a33f8e5bd346d1dde2d5add8
podlesnyy007 <114169264+podlesnyy007@users.noreply.github.com>
2022-12-02 11:53:18 +0400

    Update 1.cpp

--- 5.Вычисление конечных и бесконечных сумм и произведений/1.cpp
+++ 5.Вычисление конечных и бесконечных сумм и произведений/1.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main() 
 {
-    int n;
-    float k = 0;
+    int n; // до какого числа
+    float k = 0; // сумма
     cin >> n;
     for (int i = 1; i <= n; i++) {
         k += pow((-1), i) / pow(2, i);
