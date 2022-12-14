439b2ba2b02d603d4ee89b3d199df9697b153570
podlesnyy007 <114169264+podlesnyy007@users.noreply.github.com>
2022-12-14 20:23:32 +0400

    Create 2(б).cpp

---
+++ 6.Массивы/2(б).cpp
@@ -0,0 +1,41 @@
+
+
+#include <iostream>
+using namespace std;
+
+int **f(int& n, int& m) { // Функция создает и заполняет двумерный масссив
+    cout << "n = "; cin >> n;
+    cout << "m = "; cin >> m;
+    int **mas = new int* [n]; //Объявили двумерный динамический массив
+    for (int i = 0; i < n; i++)
+        mas[i] = new int[m];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> mas[i][j];
+        }
+    }
+    return mas;
+}
+
+int main()
+{
+    int n, m, k = 0; // k - количество отрицательный чисел
+    float s = 0; // s - сумма отрицательных чисел
+    int** a = f(n, m);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (a[i][j] < 0) {
+                s += a[i][j];
+                k++;
+            }
+        }
+    }
+    if (k != 0)
+        cout << s / k;
+    else
+        cout << "Отрицательный чисел в последовательности нет";
+    for (int i = 0; i < n; i++) // Освобождаем память, выделенную под i-ою строку массива
+        delete[] a[i]; 
+    delete[] a;// Освобождаем память, выделенную под массив указателей
+    return 0;
+}
