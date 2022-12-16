a91e87c857fa5b39bbe58efcf38d791cab4ac032
podlesnyy007 <114169264+podlesnyy007@users.noreply.github.com>
2022-12-17 00:34:32 +0400

    Update and rename 4??? to 4.cpp

---
+++ 5.Вычисление конечных и бесконечных сумм и произведений/4.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+float fan(float x, float e, int& n) {
+    float s = 0, a = 1;  // начальное значение суммы и первое слагаемое
+    n = 0;  // количество просуммированных слагаемых
+    for (int i = 2; fabs(a) >= e; i++) {
+        s += a;
+        a *= (pow(-1, i) * x) / ((i - 1) * (i + 2));
+        n++; //Увеличиваем количество просуммированных слагаемых
+    }
+    return s;
+}
+
+int main()
+{
+    float a, b, e, x, h, f; //a и b - отрезок; h - шаг
+    int i, n;
+    cout << "a = "; cin >> a;
+    cout << "b = "; cin >> b;
+    cout << "h = "; cin >> h;
+    cout << "e = "; cin >> e;
+    cout << setprecision(6);
+    cout << "i\t\tx\t\tf(x)\t\tn\n";
+    for (x = a, i = 1; x <= b; x += h, i++) {
+        f = fan(x, e, n);
+        cout << i << "\t\t" << x << "\t\t" << f << "\t\t" << n << endl;
+    }
+}
