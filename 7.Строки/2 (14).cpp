feeaf7e74917d6c440d2ce64104af28b7b8bc77d
podlesnyy007 <114169264+podlesnyy007@users.noreply.github.com>
2022-12-18 19:07:45 +0400

    Create 2 (14).cpp

---
+++ 7.Строки/2 (14).cpp
@@ -0,0 +1,24 @@
+/*
+Даны символ C и строки S, S0. После каждого вхождения символа C в строку S вставить строку S0.
+*/
+
+#include <iostream>
+#include <cstring>
+#include <string>
+using namespace std;
+
+int main() 
+{
+    char c;
+    string s, s0;
+    cout << "s = ";  getline(cin, s);
+    cout << "s0 = ";  getline(cin, s0);
+    cout << "c = "; cin >> c;
+    string res;
+    for (unsigned int i = 0; i < s.length(); i++) {
+        res += s[i];
+        if (s[i] == c)
+            res += s0;
+    }
+    cout << res;
+}
